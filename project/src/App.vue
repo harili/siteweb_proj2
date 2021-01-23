5f7ae4c211d9299e835637c0afdf270b800102ce
harili <ilieschafyfy@gmail.com>
2021-01-23 22:26:53 +0100

    Feature : Creation of components

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div id="app">
-    <h1>Bienvenue sur mon site</h1>
-  </div>
+    <b-navbar toggleable type="dark" variant="dark">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar-toggle target="navbar-toggle-collapse">
+      <template #default="{ expanded }">
+        <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+        <b-icon v-else icon="chevron-bar-down"></b-icon>
+      </template>
+    </b-navbar-toggle>
+
+    <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Link 1</b-nav-item>
+        <b-nav-item href="#">Link 2</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+    <div class="contenu container">
+      <h1>Bienvenue sur mon site</h1>
+      <div class="row">
+        <div class="col-sm-4">
+          <Card/>
+        </div>
+      </div>
+   </div>
+    
+</div>
 </template>
 
 <script>
-
-
+import Card from "@/components/Card"
 export default {
   name: 'App',
-
-}
+  components:{
+    Card,
+  }
+};
 </script>
 
 <style>
---
+++ project/src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+ <b-card
+    title="Mountain Bike"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+     Irure laborum voluptate dolor officia dolore ut et consectetur. In velit proident consequat anim duis ut nisi ut aliquip tempor proident. Ullamco sint pariatur et commodo irure fugiat irure tempor. Qui reprehenderit sint duis consequat exercitation consequat mollit est culpa.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+</template>
+
+<script>
+
+    export default {
+    
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
